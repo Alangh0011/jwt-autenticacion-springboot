ce2a801ccce9d3cc44c8387359e2b60c166c26e1
agomezh1901@alumno.ipn.mx <agomezh1901@alumno.ipn.mx>
2023-10-15 23:40:42 -0600

    Ya esta casi todo el Auth

--- src/main/java/Auth/AuthController.java
+++ src/main/java/Auth/AuthController.java
@@ -13,15 +13,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
     @PostMapping(value = "login")
-        public String login()
-        {
-            return "Login from piublic endpoint";
-        }
-    @PostMapping(value = "login")
-    public String register()
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
     {
-        return "Register from piublic endpoint";
+        return ResponseEntity.ok(authService.login(request));
     }
 
+    @PostMapping(value = "register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request)
+    {
+        return ResponseEntity.ok(authService.register(request));
+    }
 }
